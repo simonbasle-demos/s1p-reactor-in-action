d7d7dc488157cc1fe24a8d49920d64a59cc0deca
Simon Baslé <sbasle@pivotal.io>
2017-11-10 17:41:26 +0100

    Add finding of character movies by name, split the webclient code more

--- src/main/java/io/projectreactor/demos/in/action/webclient/ClientExample.java
+++ src/main/java/io/projectreactor/demos/in/action/webclient/ClientExample.java
@@ -9,6 +9,7 @@ import reactor.core.publisher.Mono;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -20,24 +21,63 @@ public class ClientExample {
 
 	private WebClient swapi = WebClient.create("http://swapi.co/api/");
 
-	@GetMapping(value = "/api/starwars/{charId}"
-//			, produces = MediaType.TEXT_EVENT_STREAM_VALUE
-	)
-	public Flux<String> foo(@PathVariable int charId) {
+	private Flux<Map> findCharacters(String characterName) {
+		//noinspection unchecked
 		return swapi.get()
-		            .uri("/people/{id}", charId)
-		            .retrieve()
-		            .bodyToMono(Map.class)
-		            .flatMapMany(json -> {
-		            	@SuppressWarnings("unchecked")
-			            Iterable<String> movieUris = (Iterable<String>) json.get("films");
-			            return Flux.fromIterable(movieUris)
-			                       .flatMap(url -> swapi.get().uri(url).retrieve().bodyToMono(Map.class))
-			                       .map(map -> map.get("title") + "\n")
-			                       .startWith("Character " + json.get("name") + " appears in movies:\n")
-			                       .concatWith(Mono.just("\nand that's it!"));
-		            });
+		         .uri("/people/?search={charName}", characterName)
+		         .retrieve()
+		         .bodyToMono(Map.class)
+		            .flatMapMany(results -> Flux.fromIterable(
+				            (List<Map>) results.get("results")));
 	}
 
+	private Mono<Map> getCharacter(int characterId) {
+		return swapi.get()
+		            .uri("/people/{id}", characterId)
+		            .retrieve()
+		            .bodyToMono(Map.class);
+	}
+
+	private Flux<String> fetchMovieTitles(Map characterJson) {
+		if (characterJson.containsKey("films")) {
+			@SuppressWarnings("unchecked")
+			Iterable<String> movieUris = (Iterable<String>) characterJson.get("films");
+			return Flux.fromIterable(movieUris)
+			           .concatMap(url -> swapi.get().uri(url)
+			                                .retrieve()
+			                                .bodyToMono(Map.class)
+			                                .map(map -> String.valueOf(map.get("title")))
+			           );
+		}
+		else {
+			return Flux.error(new IllegalArgumentException("passed Map does not represent a character"));
+		}
+	}
+
+
+
+	@GetMapping(value = "/api/starwars/person/{charId}"
+			, produces = MediaType.TEXT_EVENT_STREAM_VALUE
+	)
+	public Flux<String> charInMoviesById(@PathVariable int charId) {
+		return getCharacter(charId)
+				.flatMapMany(json -> fetchMovieTitles(json)
+						.map(title -> title + "\n")
+						.startWith("Character " + charId + " is " + json.get("name") +
+								", appears in movies:\n")
+						.concatWith(Mono.just("and that's it!\n\n"))
+				);
+	}
+
+	@GetMapping(value = "/api/starwars/person")
+	public Flux<String> charInMoviesByName(@RequestParam String name) {
+		return findCharacters(name)
+				.flatMapSequential(json -> fetchMovieTitles(json)
+						.map(title -> title + "\n")
+						.startWith("Character " + json.get("name") +
+								" appears in movies:\n")
+						.concatWith(Mono.just("and that's it!\n\n"))
+				);
+	}
 
 }
